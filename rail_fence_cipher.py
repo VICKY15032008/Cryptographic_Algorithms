0ab306ea068a6bbdec8c03b11b9e916bdbea43c6
MANDEM_VIGNESH_KUMAR_REDDY <welcometomvkr8985090256@gmail.com>
2024-11-22 15:18:08 +0530

    Update rail_fence_cipher.py

--- rail_fence_cipher.py
+++ rail_fence_cipher.py
@@ -1,7 +1,9 @@
 message=input("enter the message:")
 depth=int(input("enter the depth:"))
 def encrypt(message,depth):
+  #creation of rails
   rails=[[""for _ in range(len(message))]for _ in range(depth)]
+  #encryption technique with generalizing the principle of zig-zag order from top-bottom
   direction=1
   row,col=0,0
   for i in message:
@@ -12,13 +14,16 @@ def encrypt(message,depth):
       elif row==(depth-1):
         direction=-1
       row=row+direction
-  print(rails)   
+  print(rails)
+  #joining the text of each row and showcasing it
   encrypt="".join(["".join(rail) for rail in rails])  
   return encrypt
 
 
 def decrypt(enc_msg,depth):
+  #creation of rails
     rails=[[""for _ in range(len(enc_msg))]for _ in range(depth)]
+  #setting the positions of rails with star in zig-zag manner
     row,col=0,0
     direction=1
     
@@ -30,6 +35,7 @@ def decrypt(enc_msg,depth):
         elif row==(depth-1):
             direction=-1
         row=row+direction
+    #if the star exist then we try to give it the encrypted text-character to it  
     count=0
     print("rails at setting stage:",rails)
     for i in range(depth):
@@ -37,7 +43,8 @@ def decrypt(enc_msg,depth):
            if rails[i][j]=="*":
                 rails[i][j]=enc_msg[count]
                 count=count+1
-    print("rails at inserting stage:",rails)           
+    print("rails at inserting stage:",rails) 
+  #giving all the characters to list to make it as same as original message in order
     decrypt_msg=[]
     row=0
     col=0
@@ -49,6 +56,7 @@ def decrypt(enc_msg,depth):
         elif row==(depth-1):
             direction=-1
         row=row+direction 
+    #returning the decrypted message  
     return "".join(decrypt_msg)
 enc_msg=encrypt(message,depth)    
 print("encrypted_msg using rail fence cipher:",encrypt(message,depth))
